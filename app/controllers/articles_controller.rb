312b6a86677ac32ebd5e78dc07fa9b0780865802
adammpolak <adam.polak@citrusbyte.com>
2016-11-06 12:01:52 -0800

    remove authenticate from articles api for testing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,45 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate
+  # before_action :authenticate
 
   def index
-  articles = Article.all
-  render json: {status: 200, articles: articles}
-end
-
-def show
-  article = Article.find(params[:id])
-  render json: {status: 200, article: article}
-end
-
-def create
-  article = Article.new(article_params)
-
-  if article.save
-    render json: {status: 200, article: article}
-  else
-    render json: {status: 422, article: article, errors: article.errors}
+    articles = Article.all
+    render json: {status: 200, articles: articles}
   end
-end
 
-def update
-  article = Article.find(params[:id])
-  article.update(article_params)
+  def show
+    article = Article.find(params[:id])
+    render json: {status: 200, article: article}
+  end
 
-  render json: {status: 200, article: article}
-end
+  def create
+    article = Article.new(article_params)
 
-def destroy
-  article = Article.destroy(params[:id])
+    if article.save
+      render json: {status: 200, article: article}
+    else
+      render json: {status: 422, article: article, errors: article.errors}
+    end
+  end
 
-  render json: {status: 204}
+  def update
+    article = Article.find(params[:id])
+    article.update(article_params)
 
-end
+    render json: {status: 200, article: article}
+  end
 
-private
+  def destroy
+    article = Article.destroy(params[:id])
 
-def article_params
-  params.required(:article).permit(:title, :description, :abstract, :article_url)
-end
+    render json: {status: 204}
+
+  end
+
+  private
+
+  def article_params
+    params.required(:article).permit(:title, :description, :abstract, :article_url)
+  end
 
 
 end
